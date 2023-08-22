0e9746c0f5fa3217ed0333ff700cb3ca5cb7ffa3
Royroy205 <leroyjr2344@live.com>
2023-08-21 21:37:04 -0500

    gitignore configed with appsettings

---
+++ ORM-Dapper/IProductRepo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORM_Dapper
+{
+    public interface IProductRepo
+    {
+      public IEnumerable<Product> GetProducts();
+
+        public void InsertProduct(string name, double price, int categoryID );
+    
+    
+    }
+}
---
+++ ORM-Dapper/ProductRepo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORM_Dapper
+{
+    public class ProductRepo 
+    {
+        private readonly IDbConnection connection;
+
+        public ProductRepo(IDbConnection _connection) 
+        { 
+           _connection = connection;
+        }
+       
+        public IEnumerable<Product> GetProducts() 
+        {
+            return _connection.Query<Product>("SELECT * FROM Products;");
+        
+        }
+    
+        public void InsertProduct(string name, double price, int categoryID) 
+        { 
+        
+        
+        }
+    }
+}
